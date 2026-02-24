f29409fcd8abd28cedd00d5a30e3981aa0bb6247
Juan Gro <softdevjuanca@gmail.com>
2026-02-24 15:45:04 -0600

    fix custom-sw

--- frontend_chantli/public/custom-sw.js
+++ frontend_chantli/public/custom-sw.js
@@ -1,28 +1,24 @@
-// public/custom-sw.js
+// Chantli/frontend_chantli/public/custom-sw.js
 
-// 1. Escuchar cuando llega un mensaje Push del servidor
 self.addEventListener('push', function(event) {
     // Si el servidor mandó datos, los extraemos
     const data = event.data ? event.data.json() : {};
     
-    const title = data.title || 'Nueva Notificación de Chantli';
+    // OJO AQUÍ: Cambiamos data.title por data.head (así lo manda Django)
+    const title = data.head || 'Nueva Notificación de Chantli';
     const options = {
         body: data.body || 'Tienes una actualización.',
-        icon: '/pwa-192x192.png', // Tu logo
-        badge: '/pwa-192x192.png', // Icono pequeño monocromático para la barra de estado
-        vibrate: [200, 100, 200], // Patrón de vibración del celular
-        data: data.url || '/' // ¿A dónde llevamos al usuario si toca la notificación?
+        icon: '/pwa-192x192.png',
+        badge: '/pwa-192x192.png',
+        vibrate: [200, 100, 200],
+        data: data.url || '/' 
     };
 
-    // Le decimos al celular que dibuje la notificación nativa
     event.waitUntil(self.registration.showNotification(title, options));
 });
 
-// 2. Escuchar cuando el usuario TOCA la notificación
 self.addEventListener('notificationclick', function(event) {
-    event.notification.close(); // Cerramos la alerta
-    
-    // Abrimos la app en la ruta que venía en los datos
+    event.notification.close();
     event.waitUntil(
         clients.openWindow(event.notification.data)
     );
