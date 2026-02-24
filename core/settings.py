1180376e7002b5b2c059b79ad1c151ae8c4ed3ba
Juan Gro <softdevjuanca@gmail.com>
2026-02-24 10:44:56 -0600

    mod a settings para llegar a los static

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ env = environ.Env(
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 STATIC_ROOT = '/var/www/Chantli/staticfiles/'
+STATIC_URL = 'static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -139,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
